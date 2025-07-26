85ab399d40413be8f55651f50b87acb8741c0a94
GuptTmay <tanmaygupta22003@gmail.com>
2025-07-26 23:10:55 +0530

    router fix

--- frontend/src/pages/Hero.tsx
+++ frontend/src/pages/Hero.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { useNavigate } from "react-router-dom"
 
 const Hero = () => {
+  const navigate = useNavigate();
   return (
-    <div>Hero</div>
+    <div>Hero
+
+      <Button onClick={() => navigate('/feeds')}>go to feed</Button>
+    </div>
   )
 }
 
