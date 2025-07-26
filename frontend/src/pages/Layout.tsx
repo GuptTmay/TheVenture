285de3c1d464178cded7008869ce64d254770553
GuptTmay <tanmaygupta22003@gmail.com>
2025-07-26 22:25:58 +0530

    router fix

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,9 +1,11 @@
 import { ModeToggle } from "@/components/ModeToggle"
+import { Outlet } from "react-router-dom"
 
 const Layout = () => {
   return (
     <div>Layout
       <ModeToggle />
+      <Outlet/>
     </div>
   )
 }
